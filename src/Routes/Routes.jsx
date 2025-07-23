eeeff744c8f59167a97306d73aa254ec76e1acc5
Arham Riaz <riaz@bridge-wireless.com>
2025-07-23 13:21:36 -0400

    removed Admin Login related files

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,36 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../components/Customers/Pages/Home";
 import Contact from "../components/Customers/Pages/Contact";
 import NoPage from "../components/Customers/Pages/NoPage";
-import Dashboard from "../components/Admin/Pages/Dashboard";
-import AdminLayout from "../components/Admin/Layout/AdminLayout";
-import Users from "../components/Admin/Pages/Users";
-import Customers from "../components/Admin/Pages/Customers";
-import CustomersList from "../components/Admin/Pages/CustomersList";
-import Devices from "../components/Admin/Pages/Devices";
-import Trade from "../components/Admin/Pages/Trade";
-import Reports from "../components/Admin/Pages/Reports";
-import ProtectedRoute from "../components/Admin/Pages/ProtectedRoute"; // Import ProtectedRoute
 import TradeLanding from "../components/Customers/Pages/TradeLanding";
 import About from "../components/Customers/Pages/About";
 import TermsConditions from "../components/Customers/Pages/TermsConditions";
 import TradeinForm from "../components/Customers/Pages/TradeinForm";
 import CustomerInfoForm from "../components/Customers/Pages/CustomerInfoForm";
-import Incentive from "../components/Admin/Pages/Incentive";
-import UsersList from "../components/Admin/Pages/UsersList";
-import Manufacturer from "../components/Admin/Pages/Manufacturer";
-import Model from "../components/Admin/Pages/Model";
-import Memory from "../components/Admin/Pages/Memory";
-import MemoryList from "../components/Admin/Pages/MemoryList";
-import TradeInPage from "../components/Admin/Pages/TradeInPage";
-import TradeHistory from "../components/Admin/Pages/TradeHistory";
-import ModelList from "../components/Admin/Pages/ModelList";
-import ManufacturerList from "../components/Admin/Pages/ManufacturerList";
 import HomeLayout from "../components/Customers/Layout/HomeLayout";
 import TradeList from "../components/Customers/Pages/TradeHistory";
 import AuthLayout from "../components/Customers/Layout/AuthLayout";
 import PrivacyPolicy from "../PrivacyPolicy";
 import CookiePolicy from "../CookiePolicy";
-import Batch from "../components/Admin/Pages/addBatch";
 import AddTrade from "../components/Trade/AddTrade";
 import CompetitivePricing from "../components/Customers/Pages/CompetitivePricing;";
 export default function AppRoutes() {
@@ -59,30 +39,6 @@ export default function AppRoutes() {
       </Route>
 
       <Route path="/" element={<AuthLayout />}></Route>
-
-      {/* Protected Admin Routes - Do we need these?*/}
-      <Route path="/admin" element={<ProtectedRoute />}>
-        <Route element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="devices" element={<Devices />} />
-          <Route path="users/add" element={<Users />} />
-          <Route path="trade" element={<Trade />} />
-          <Route path="reports" element={<Reports />} />
-          <Route path="customers/add" element={<Customers />} />
-          <Route path="customers" element={<CustomersList />} />
-          <Route path="incentive" element={<Incentive />} />
-          <Route path="users" element={<UsersList />} />
-          <Route path="manufacturer/add" element={<Manufacturer />} />
-          <Route path="manufacturer" element={<ManufacturerList />} />
-          <Route path="models/add" element={<Model />} />
-          <Route path="memory/add" element={<Memory />} />
-          <Route path="memory" element={<MemoryList />} />
-          <Route path="trade-in" element={<TradeInPage />} />
-          <Route path="trade-history" element={<TradeHistory />} />
-          <Route path="models" element={<ModelList />} />
-          <Route path="batch" element={<Batch />} />
-        </Route>
-      </Route>
 
       {/* Fallback Route */}
       <Route path="*" element={<NoPage />} />
--- src/components/Customers/Pages/About.jsx
+++ src/components/Customers/Pages/About.jsx
@@ -1,148 +1,138 @@
-import React from 'react';
-import '../Layout/About.css'; // Optional for any additional custom CSS
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import { FaRecycle, FaHandsHelping, FaMobileAlt } from 'react-icons/fa';
-import { motion } from 'framer-motion'; // Import Framer Motion
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next'; // Import i18n hook
+import React from "react";
+import "../Layout/About.css"; // Optional for any additional custom CSS
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { FaRecycle, FaHandsHelping, FaMobileAlt } from "react-icons/fa";
+import { motion } from "framer-motion"; // Import Framer Motion
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next"; // Import i18n hook
 const About = () => {
-    const navigate = useNavigate(); // Initialize navigate
-    const { t } = useTranslation('about'); // Load the 'navigation' namespace
+  const navigate = useNavigate(); // Initialize navigate
+  const { t } = useTranslation("about"); // Load the 'navigation' namespace
 
-    // Animation Variants
-    const fadeVariants = {
-        hidden: { opacity: 0, y: -50 },
-        visible: { opacity: 1, y: 0, transition: { duration: 1 } },
-    };
+  // Animation Variants
+  const fadeVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
 
-    const zoomVariants = {
-        hidden: { scale: 0 },
-        visible: { scale: 1, transition: { duration: 1 } },
-    };
+  const zoomVariants = {
+    hidden: { scale: 0 },
+    visible: { scale: 1, transition: { duration: 1 } },
+  };
 
-    return (
-        <>
-            <section className="slider text-white bg-dark py-5 mt-5">
-                <Container>
-                    <Row className="justify-content-center text-center mt-5">
-                        <Col lg={9} md={12}>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                variants={fadeVariants}
-                            >
-                                <h1 className="animated fadeInUp mb-3 mt-5 text-white">{t('our_mission_title')}</h1>
-                                <p className="lead text-white mb-4">
-                                   {t('our_mission_description')}
-                                </p>
-                            </motion.div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
+  return (
+    <>
+      <section className="slider text-white bg-dark py-5 mt-5">
+        <Container>
+          <Row className="justify-content-center text-center mt-5">
+            <Col lg={9} md={12}>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={fadeVariants}
+              >
+                <h1 className="animated fadeInUp mb-3 mt-5 text-white">
+                  {t("our_mission_title")}
+                </h1>
+                <p className="lead text-white mb-4">
+                  {t("our_mission_description")}
+                </p>
+              </motion.div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
-            <Container className="my-5" fluid="xl">
-                <Row className="text-center mb-4">
-                    <Col>
-                        <h2>{t('our_values_title')}</h2>
-                        <p>{t('our_values_description')}</p>
-                    </Col>
-                </Row>
+      <Container className="my-5" fluid="xl">
+        <Row className="text-center mb-4">
+          <Col>
+            <h2>{t("our_values_title")}</h2>
+            <p>{t("our_values_description")}</p>
+          </Col>
+        </Row>
 
-                <Row className="g-4">
-                    {[
-                        {
-                            icon: <FaHandsHelping size={40} className="text-success mb-3" />,
-                            title: t('values.expertise_title'),
-                            text: t('values.expertise_description'),
-                        },
-                        {
-                            icon: <FaMobileAlt size={40} className="text-success mb-3" />,
-                            title: t('values.quality_title'),
-                            text: t('values.quality_description'),
-                        },
-                        {
-                            icon: <FaRecycle size={40} className="text-success mb-3" />,
-                            title: t('values.sustainability_title'),
-                            text: t('values.sustainability_description'),
-                        },
-                    ].map((value, index) => (
-                        <Col md={4} key={index}>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                variants={zoomVariants}
-                                className="card shadow-sm p-3 h-100"
-                            >
-                                <div className="card-body">
-                                    {value.icon}
-                                    <h4 className="card-title">{value.title}</h4>
-                                    <p className="card-text">{value.text}</p>
-                                </div>
-                            </motion.div>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+        <Row className="g-4">
+          {[
+            {
+              icon: <FaHandsHelping size={40} className="text-success mb-3" />,
+              title: t("values.expertise_title"),
+              text: t("values.expertise_description"),
+            },
+            {
+              icon: <FaMobileAlt size={40} className="text-success mb-3" />,
+              title: t("values.quality_title"),
+              text: t("values.quality_description"),
+            },
+            {
+              icon: <FaRecycle size={40} className="text-success mb-3" />,
+              title: t("values.sustainability_title"),
+              text: t("values.sustainability_description"),
+            },
+          ].map((value, index) => (
+            <Col md={4} key={index}>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={zoomVariants}
+                className="card shadow-sm p-3 h-100"
+              >
+                <div className="card-body">
+                  {value.icon}
+                  <h4 className="card-title">{value.title}</h4>
+                  <p className="card-text">{value.text}</p>
+                </div>
+              </motion.div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
 
-            <Container className="my-5 mt-5" fluid="xl">
-                <Row>
-                    <Col md={6} className="mb-5">
-                        <motion.div
-                            initial="hidden"
-                            animate="visible"
-                            variants={fadeVariants}
-                        >
-                            <h3>{t('why_trade_in_title')}</h3>
-                            <p>
-                                {t('why_trade_in_description')}
-                            </p>
-                        </motion.div>
-                    </Col>
+      <Container className="my-5 mt-5" fluid="xl">
+        <Row>
+          <Col md={6} className="mb-5">
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={fadeVariants}
+            >
+              <h3>{t("why_trade_in_title")}</h3>
+              <p>{t("why_trade_in_description")}</p>
+            </motion.div>
+          </Col>
 
-                    <Col md={6} className="mb-5">
-                        <motion.div
-                            initial="hidden"
-                            animate="visible"
-                            variants={fadeVariants}
-                        >
-                            <h3>{t('how_it_works_title')}</h3>
-                            <ol>
-                                <li>{t('how_it_works_steps.step1')}</li>
-                                <li>{t('how_it_works_steps.step2')}</li>
-                                <li>{t('how_it_works_steps.step3')}</li>
-                            </ol>
-                        </motion.div>
-                    </Col>
-                </Row>
-            </Container>
+          <Col md={6} className="mb-5">
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={fadeVariants}
+            >
+              <h3>{t("how_it_works_title")}</h3>
+              <ol>
+                <li>{t("how_it_works_steps.step1")}</li>
+                <li>{t("how_it_works_steps.step2")}</li>
+                <li>{t("how_it_works_steps.step3")}</li>
+              </ol>
+            </motion.div>
+          </Col>
+        </Row>
+      </Container>
 
-            <Container className="my-5 text-center " fluid="xl">
-                <Row>
-                    <Col>
-                        <motion.div
-                            initial="hidden"
-                            animate="visible"
-                            variants={fadeVariants}
-                        >
-                            <h3>{t('join_us_title')}</h3>
-                            <p>
-                                {t('join_us_description')}
-                            </p>
-                            <Button
-                                variant=""
-                                className="btn-success text-white"
-                                size="lg"
-                                onClick={() => navigate('/login')}
-                            >
-                                {t('get_started')}
-                            </Button>
-                        </motion.div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+      <Container className="my-5 text-center " fluid="xl">
+        <Row>
+          <Col>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={fadeVariants}
+            >
+              <h3>{t("join_us_title")}</h3>
+              <p>{t("join_us_description")}</p>
+            </motion.div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default About;
